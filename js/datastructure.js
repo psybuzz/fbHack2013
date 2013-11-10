4210137821a6c6de9b625accffbd1cb50f7e84b0
Ken Tian <ken.yht@gmail.com>
2013-11-09 23:03:24 -0600

    data structure update

--- js/datastructure.js
+++ js/datastructure.js
@@ -2,7 +2,10 @@ function picture() {
 	return {
 		width: 0,
 		height: 0, 
-		pixel[],
+		filled[],
+		percentage: 0,
+		num_pixels: 0,
+		num_filled: 0
 
 		function initilaize(im_width, im_height) {
 			this.width = im_width;
@@ -10,21 +13,35 @@ function picture() {
 			for (var i = 0; i < im_width; i++) {
 				var row = [];
 				for (var j = 0; j < im_height; j++) {
-					row.push(new node(canvasOverlay.getImageData(i, j, 1, 1).data, i, j));
+					row.push(0);
 				}
-				pixel.push(row);
+				filled.push(row);
+			}
+			num_pixels = im_width * im_height;
+		}
+
+		function update() {
+			for (var i = 0; i < this.width; i++) {
+				for (var j = 0; j < this.height; j++) {
+					var curr_pixel = overlayContext.getImageData(i, j, 1, 1).data;
+					if (curr_pixel[0] != 0 || curr_pixel[1] != 0 || curr_pixel[2] != 0) {
+						filled[i][j] = 1;
+						num_filled++;
+					}
+				}
 			}
 		}
 
-
+		function checkDone() {
+			return num_filled >= (num_pixels * 9 / 10);
+		}
 	}
 }
-
+/*
 function node(pixel_data, x_loc, y_loc) {
 	return {
 		x: x_loc,
 		y: y_loc,
-		filled: 0,
-		data: pixel_data
+		filled: 0
 	}
-}
+}*/
